2affbd164b20781e9c2bf6fee6ce6a6ad4fe2ab2
Osuolale Marvellous <31046066+mahveotm@users.noreply.github.com>
2019-03-10 04:29:10 +0100

    Update Tweepedia.py
    tweaks

--- Tweepedia.py
+++ Tweepedia.py
@@ -1,10 +1,12 @@
-#Essentially, this bot will take the content of a tweet it is mentioned and check against Wikipedia and
-# returns snippet of the article with link to continue reading.
+#Essentially, this bot will take the content of a tweet it is mentioned and check against Wikipedia if a content
+#with ssuch topic exists. It then returns snippets of the article if it exists with a link to continue reading if
+#you so wish. Does not currently support wrong casing i.e if you write lagos state university instead of Lagos State
+#University, you will probably not get any result. Open to collaboration and ideas. I gladly welcome then!
 import tweepy
 import requests
 import urllib.parse
 import time
-#concumer ID's -When you create an app with developers account, you will be issued these
+#consumer ID's - When you create an app on twitter, you will be issued these
 CONSUMER_KEY ='consumer key goes here'
 CONSUMER_SECRET ='consumer secret goes here'
 ACCESS_KEY ='access key goes here'
@@ -30,8 +32,6 @@ def store_last_seen_id(last_seen_id, file_name):
     return
 
 def tweepedia():
-
-
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
     #reading the mentions and responding
     mentions = api.mentions_timeline(
@@ -41,14 +41,14 @@ def tweepedia():
         print(str(mention.id) + ' - ' + mention.full_text)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        mtn = mention.full_text
-        glo = str(mtn.strip().lstrip("@mahveo_"))
-        article = str(glo.strip().lstrip())
-        airtel =article.replace(" ", "_")
+        foo = mention.full_text
+        bar = str(foo.strip().lstrip("@mahveo_"))
+        article = str(bar.strip().lstrip())
+        foobar =article.replace(" ", "_")
         link = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
         web = 'https://en.wikipedia.org/wiki/'
         url = link + article + '?redirect=true'
-        website = web + airtel
+        website = web + foobar
         json_data = requests.get(url).json()
         json_extract = json_data['extract']
         data= json_extract
